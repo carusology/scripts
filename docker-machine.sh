c87b9a668e5984ae7540ff0fc62dcf3c6d211fa7
Brian Caruso <carusobp@gmail.com>
2016-05-25 10:07:13 -0700

    Add 'getenv' to check the current docker-machine variables

--- docker-machine.sh
+++ docker-machine.sh
@@ -1,5 +1,12 @@
+# Some bash functions to manage swapping back and forth between
+# docker-machine and docker for mac
+# Install via "source ./path/docker-machine.sh"
+
 # Sets environment variables set for docker-machine
 # so I can run docker commands via my docker-machine
+
+declare -a docker_machine_variables
+
 function setenv() {
     if [[ -z $1 ]]; then
 	echo
@@ -16,11 +23,10 @@ function setenv() {
     eval $(docker-machine env $1)
 }
 
-# Unsets environment variables set for docker-machine
-# so I can quickly swap back to docker for mac
-function unsetenv() {
-    local variables=($(env | cut -d'=' -f1 | grep "DOCKER_"))
-    if [ "${#variables[@]}" -eq 0 ]; then
+function getenv() {
+    get_docker_machine_variables
+    
+    if [ "${#docker_machine_variables[@]}" -eq 0 ]; then
 	echo 
 	echo "  No docker-machine environment variables have been set."
 	echo
@@ -29,9 +35,36 @@ function unsetenv() {
     fi
 
     echo
-    echo "  Unsetting ${#variables[@]} docker-machine environment variables:"
+    echo "  Current docker-machine environment variables:"
+
+    for variable in "${docker_machine_variables[@]}"
+    do
+        echo "    $variable = $(printenv $variable)"
+    done
+
+    echo
     
-    for variable in "${variables[@]}"
+    return 0
+}
+
+
+# Unsets environment variables set for docker-machine
+# so I can quickly swap back to docker for mac
+function unsetenv() {
+    get_docker_machine_variables    
+
+    if [ "${#docker_machine_variables[@]}" -eq 0 ]; then
+	echo 
+	echo "  No docker-machine environment variables have been set."
+	echo
+	
+	return 1
+    fi
+
+    echo
+    echo "  Unsetting ${#docker_machine_variables[@]} docker-machine environment variables:"
+    
+    for variable in "${docker_machine_variables[@]}"
     do
         echo "    unsetting $variable ..."
 	unset $variable
@@ -42,5 +75,11 @@ function unsetenv() {
     return 0
 }
 
+# Returns an array of environment
+function get_docker_machine_variables() {
+    docker_machine_variables=($(env | cut -d'=' -f1 | grep "DOCKER_"))
+}
+
 export -f setenv
+export -f getenv
 export -f unsetenv
